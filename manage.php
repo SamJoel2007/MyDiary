11d5a61713f73d014e26af5e224cbf5d65ab6f70
SamJoel2007 <your_github_email@gmail.com>
2026-05-07 10:44:26 +0530

    Initial Commit

---
+++ manage.php
@@ -0,0 +1,33 @@
+<?php 
+
+$action = $_POST['action'] ?? '';
+
+function register($email, $password) {
+    echo "Working on it";
+}
+
+function login() {
+    echo "Working on it";
+}
+
+function publish() {
+    echo "Working on it";
+}
+
+function edit_profile() {
+    echo "Working on it";
+}
+
+function delete_entry() {
+    echo "Working on it";
+}
+
+// MAIN 
+
+if ($action == "1") {
+    echo "Working on it";
+} else {
+    echo "Invalid ACTION!";
+}
+
+?>
